62c634e81095e95f3448f0036f78119f8e6c251f
TsT <tst2005@gmail.com>
2018-07-20 16:15:39 +0200

    brutforce on a reduce alphabet

--- brutforce.lua
+++ brutforce.lua
@@ -2,22 +2,45 @@
 
 local amhash = require "amhash".amhash
 
-local ascii = {""}
+local ascii = {}
 for i=0,255 do
 	table.insert(ascii, string.char(i))
 end
+-- ascii = { "\0", ... "a", "b", ... }
 
--- ascii = { "", "\0", ... "a", "b", ... }
+local ascii = {"\t", "\n", "\r"}
+--             0x09  0x0a  0x0d
+for i=0x20,0x7e do
+	table.insert(ascii, string.char(i))
+end
 
---for len=1,5 do
+local function printable(data)
+	return (#(string.gsub(data, ".", function(c)
+		local v = string.byte(c)
+		if v == 0x09 or v == 0x0a or v == 0x0d or ( v >= 0x20 and v <= 0x7e ) then
+			return ""
+		else
+			return "x"
+		end
+	end))==0)
+end
+
+--print ( printable("abc") )
+--print ( printable("a\0bc") )
+--print ( printable("foo é bar") )
+
+
 local function brutforce(prefix, ascii, i, j)
 	for _i, c in ipairs(ascii, i, j) do
-		print(amhash(prefix..c))
+		print(amhash(prefix..c)..":", string.byte(prefix..c, 1, -1))
 	end
 end
 
-for _i, p0 in ipairs(ascii) do
-	for _i, p1 in ipairs(ascii) do
-		brutforce(p0..p1, ascii)
+io.stderr:write("alphabet len: "..#ascii.."\n")
+for _i, p1 in ipairs(ascii) do
+	for _i, p2 in ipairs(ascii) do
+		for _i, p3 in ipairs(ascii) do
+			brutforce(p1..p2..p3, ascii)
+		end
 	end
 end
